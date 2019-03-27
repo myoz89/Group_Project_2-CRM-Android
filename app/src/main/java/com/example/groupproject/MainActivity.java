1530316e2aad4127a2a3d5ce65932c484e227094
ZarniMa <zarnipyaesone@gmail.com>
2019-03-27 10:55:41 -0700

    i add codes for sign in button user can sign in as owner or customer it will bring to the main menu activity belong with the user id.

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -6,12 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.IOException;
 
 import Controller.IO;
+import Controller.SignIn;
 import Model.AllUsers;
+import Model.Customer;
+import Model.Owner;
 
 import static Controller.IO.readFromFile;
 
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private Button butSignIn;
     private Button butSignUp;
     private AllUsers allUsers;
+    private EditText userID,password; // user input for signin
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +41,53 @@ public class MainActivity extends AppCompatActivity {
             allUsers = new AllUsers();
         }
 
+        // get data from edit text
+        userID = findViewById(R.id.user_name);
+        password = findViewById(R.id.password);
+
         //Set up the buttons
         //Sign In
         butSignIn = findViewById(R.id.sign_in);
         butSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // set up the userid and password as the string that get from edit text
+                String sUserid = userID.getText().toString();
+                String sPassword = password.getText().toString();
+
+                // check account exist and check sing in as owner or customer
+                Owner owner = allUsers.getOwnerBasedOnID(sUserid);
+                Customer customer = allUsers.getCustomerBasedOnID(sUserid);
+
+                if (owner != null && customer == null){
+                    // sign in as owner
+                    boolean isSignin = SignIn.Signin(sUserid,sPassword,allUsers);
+                    if (isSignin){ // if isSignin is false, wrong password
+                        // if sucessfully sing in, go to owner main menu
+                        Intent intent = new Intent(MainActivity.this, OwnerMainMenuActivity.class);
+                        startActivity(intent);
+                    }
+                    // in later, i like to show error msg when user put wrong password
+
+                }
+                else {
+                    Toast.makeText(getBaseContext(),"wrong user name or haven't sing up yet!",Toast.LENGTH_SHORT).show();
+                }
+
+                if (customer != null && owner == null){
+                    // sign in as customer
+                    boolean isSignin = SignIn.Signin(sUserid,sPassword,allUsers);
+                    if (isSignin) {
+                        // if sucessfully sing in, go to owner main menu
+                        Intent intent = new Intent(MainActivity.this, CustomerMainMenuActivity.class);
+                        startActivity(intent);
+                    }
+                    // in later, i like to show error msg when user put wrong password
+                }
+                else {
+                    Toast.makeText(getBaseContext(),"wrong user name or haven't sing up yet!",Toast.LENGTH_SHORT).show();
+
+                }
 
             }
         });
